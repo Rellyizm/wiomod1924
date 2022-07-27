66e969a210a91d9fc570c35278b3af6a2be42931
Rellyizm <79768100+Rellyizm@users.noreply.github.com>
2022-07-27 12:03:09 +0300

    Update [5]-Say.js

--- Executive/Commands/[5]-Say.js
+++ Executive/Commands/[5]-Say.js
@@ -10,16 +10,16 @@ module.exports.run = async(client, message, args, embed) => {
     let BanYetkili = data.BanYetkiliRol;
 
 if (!BanYetkili.some(x => message.member.roles.cache.has(x)) && !message.member.roles.cache.has(BotOwner)) return 
-    let top = message.guild.memberCount;
+     var üyesayısı = message.guild.members.cache.size;
     let tagges = message.guild.members.cache.filter(s => data.TAG.some(a => s.user.tag.toLowerCase().includes(a))).size
     let ses = message.guild.members.cache.filter(s => s.voice.channel).size
     let boost = message.guild.premiumSubscriptionCount;
     let boostlevel = message.guild.premiumTier == "NONE" ? 0 : message.guild.premiumTier;
-    let online = message.guild.members.cache.filter(s => s.presence?.status != "offline").size
+    var atkifler = message.guild.members.cache.filter(m => m.presence && m.presence.status !== "offline").size
 
     message.channel.send({embeds: [embed.setDescription(`
     ❯ Şuan da toplam **${ses}** kişi ses kanallarında aktif.
-    ❯ Sunucu da şuan da toplam **${top}** üye var (**${online}** Aktif).
+    ❯ Sunucu da şuan da toplam **${üyesayısı}** üye var (**${atkifler}** Aktif).
     ❯ Toplam **${tagges}** kişi tagımızı alarak bize destek oluyor. 
     ❯ Sunucumuz şuan da  **3** seviye ve **${boost}** boost basılmış.`)]}) 
 
