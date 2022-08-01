105d7110aef68c9b6d66771c68b161d504b38042
Rellyizm <79768100+Rellyizm@users.noreply.github.com>
2022-08-01 10:25:54 +0300

    Update [4]Me.js

--- Executive/Commands/[4]Me.js
+++ Executive/Commands/[4]Me.js
@@ -19,13 +19,6 @@ const inviteSchema = require('../../Inv/models/invite');
 const statData = require('../Models/Stats')
 
 module.exports.run = async (client, message, args) => {
-    let data = await sunucuVeri.findOne({
-        guildID: message.guild.id
-    });
-    let BotOwner = data.BOTOWNER;
-    let BanYetkili = data.BanYetkiliRol;
-
-if (!BanYetkili.some(x => message.member.roles.cache.has(x)) && !message.member.roles.cache.has(BotOwner)) return 
         const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
         const row = new Discord.MessageActionRow()
         .addComponents(
